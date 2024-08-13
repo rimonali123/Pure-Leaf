24d522a431bc4e9c0327a23c6d62f80560e8aa81
Rimon Ali <rimonali450@gmail.com>
2024-08-13 16:10:49 +0600

    about us page offer section added

--- src/Pages/AboutUs/AboutUsPage.jsx
+++ src/Pages/AboutUs/AboutUsPage.jsx
@@ -1,4 +1,6 @@
-
+import { PiPottedPlant } from "react-icons/pi";
+import { MdOutlineSupportAgent } from "react-icons/md";
+import { GiTreeGrowth } from "react-icons/gi";
 
 const AboutUsPage = () => {
     return (
@@ -13,7 +15,56 @@ const AboutUsPage = () => {
                     e-commerce website platform
                 </h1>
                 <p className="text-4xl font-medium absolute top-[440px] lg:top-96 lg:left-[120px] text-black bg-white p-4 rounded-full opacity-80">Every farmer strives to find a
-                good value to their crops.</p>
+                    good value to their crops.</p>
+            </div>
+
+            <div className="mt-10  p-10 bg-slate-100">
+
+                <div className="w-1/2 mx-auto space-y-5 text-center">
+                    <h1 className="text-5xl font-medium">What we offers?</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error recusandae quam exercitationem, et reprehenderit facilis aspernatur culpa nisi eligendi, a obcaecati commodi iusto provident consectetur quod corrupti veniam aperiam atque quo beatae est blanditiis architecto molestias maiores. Possimus, minus ipsa?</p>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 mt-10 w-full">
+
+                    <div className="bg-white rounded-2xl p-6  dark:bg-gray-50 dark:text-gray-900" >
+                        <div className="">
+                            <PiPottedPlant className="text-9xl text-green-500 text-center w-full" />
+                        </div>
+                        <div className="mt-6 mb-2" >
+
+                            <h2 className="text-xl font-semibold tracking-wide text-center" >Farm Services</h2>
+                        </div>
+                        <p className="dark:text-gray-800 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at pellentesque diam.</p>
+                    </div>
+
+
+                    <div className="bg-white rounded-2xl p-6  dark:bg-gray-50 dark:text-gray-900" >
+                        <div className="">
+                            <MdOutlineSupportAgent className="text-9xl text-green-500 text-center w-full" />
+                        </div>
+                        <div className="mt-6 mb-2" >
+
+                            <h2 className="text-xl font-semibold tracking-wide text-center" >Farm Services</h2>
+                        </div>
+                        <p className="dark:text-gray-800 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at pellentesque diam.</p>
+                    </div>
+
+
+                    <div className="bg-white rounded-2xl p-6  dark:bg-gray-50 dark:text-gray-900" >
+                        <div className="">
+                            <GiTreeGrowth className="text-9xl text-green-500 text-center w-full" />
+                        </div>
+                        <div className="mt-6 mb-2" >
+
+                            <h2 className="text-xl font-semibold tracking-wide text-center" >Farm Services</h2>
+                        </div>
+                        <p className="dark:text-gray-800 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at pellentesque diam.</p>
+                    </div>
+
+
+
+                </div>
             </div>
         </div>
     );
