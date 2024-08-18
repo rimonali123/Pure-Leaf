0e5d3b99b6a3c4fd65e5f8ea59d695c54550f85a
Rimon Ali <rimonali450@gmail.com>
2024-08-18 12:27:27 +0600

    address section implement successfully

--- src/Pages/AccountDashboard/DashboardAddressPage/DashboardAddressPage.jsx
+++ src/Pages/AccountDashboard/DashboardAddressPage/DashboardAddressPage.jsx
@@ -3,7 +3,24 @@
 const DashboardAddressPage = () => {
     return (
         <div>
-            this is address page
+            <h3 className=" p-4">The following addresses will be used on the checkout page by default.</h3>
+            <div className="flex gap-5 items-center w-full">
+                <div className="w-full border">
+                    <div className="flex justify-between items-center bg-slate-50 border">
+                        <h4 className="bg-slate-100 text-xl font-semibold p-4">Billing address</h4>
+                        <button className="p-4 hover:text-green-500">Add</button>
+                    </div>
+                    <p className="text-center py-4">You have not set up this type of address yet.</p>
+                </div>
+
+                <div className="w-full border">
+                    <div className="flex justify-between items-center bg-slate-50 border">
+                        <h4 className="bg-slate-100 text-xl font-semibold p-4">Shipping address</h4>
+                        <button className="p-4 hover:text-green-500">Add</button>
+                    </div>
+                    <p className="text-center py-4">You have not set up this type of address yet.</p>
+                </div>
+            </div>
         </div>
     );
 };
